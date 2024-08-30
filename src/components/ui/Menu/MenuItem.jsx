6f3927041f8cece2f32b28814111088e57942d50
zcley02 <zcleymerjoel16@gmail.com>
2024-08-29 22:55:58 -0600

    Second Progress in Navbar/Header

--- src/components/ui/Menu/MenuItem.jsx
+++ src/components/ui/Menu/MenuItem.jsx
@@ -7,7 +7,18 @@ export default function MenuItem( {option} ) {
         <Link
             position = "relative"
             w = "120px"
+            px="0"
+            opacity="0.8"
+            display="flex"
+            alignItems="center"
+            borderBottom="2px solid transparent"
             justifyContent = { desktopView && "center" }
+            _hover={{
+                opacity: "1",
+                fontWeight: "bold",
+                borderBottom: "2px solid #536DFE",
+            }}
+            href={`#${option.ref}`}
         >
             {option.text}
         </Link>
--- src/components/ui/Menu/MenuOption.jsx
+++ src/components/ui/Menu/MenuOption.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link, useMediaQuery, Image } from "@chakra-ui/react";
 
 import MenuItem from "./MenuItem";
+import PageStore from "@src/store/PageStore";
 
 export default function MenuOptions() {
     const [ menuOptions, setMenuOptions ] = useState([]);
@@ -9,26 +10,28 @@ export default function MenuOptions() {
 
     const [desktopView] = useMediaQuery("(min-width: 1000px)");
 
-    const language = "spanish";
+    const language = PageStore((state) => state.language);
+    const changeLanguage = PageStore((state) => state.changeLanguage);
     
     const spanish = [
-        { ref: "start", text: "Inicio" },
-        { ref: "about", text: "Sobre mi" },
-        { ref: "skills", text: "Habilidades" },
-        { ref: "certificates", text: "Certificados" },
-        { ref: "projects", text: "Proyectos" },
-        { ref: "experiencie", text: "Experiencia" },
-        { ref: "contact", text: "Contacto" },
-      ];
-      const english = [
-        { ref: "start", text: "Home" },
-        { ref: "about", text: "About" },
-        { ref: "skills", text: "Skills" },
-        { ref: "certificates", text: "Certificados" },
-        { ref: "projects", text: "Proyects" },
-        { ref: "experiencie", text: "Experiencie" },
-        { ref: "contact", text: "Contact" },
-      ];
+      { ref: "start", text: "Inicio" },
+      { ref: "about", text: "Sobre mi" },
+      { ref: "skills", text: "Habilidades" },
+      { ref: "certificates", text: "Certificados" },
+      { ref: "projects", text: "Proyectos" },
+      { ref: "experiencie", text: "Experiencia" },
+      { ref: "contact", text: "Contacto" },
+    ];
+      
+    const english = [
+      { ref: "start", text: "Home" },
+      { ref: "about", text: "About" },
+      { ref: "skills", text: "Skills" },
+      { ref: "certificates", text: "Certificates" },
+      { ref: "projects", text: "Proyects" },
+      { ref: "experiencie", text: "Experiencie" },
+      { ref: "contact", text: "Contact" },
+    ];
 
     
     useEffect(() => {
@@ -40,7 +43,24 @@ export default function MenuOptions() {
             {menuOptions.map((option, index) => {
                 return <MenuItem key={index} option={option}/>;
             })}
+            <Link
+              display="flex"
+              alignItems="center"
+              w="60px" 
+              opacity={0.8}
+              _hover={{ opacity: "1" }}
+              _active={{}}
+              onClick={(e) => {
+                e.stopPropagation();
+                changeLanguage(language);
+              }}
+            >
+              <span>{!desktopView && (language ? "Es" : "En")}</span>
+              <Image src={flag} h="10px" mx="10px" pointerEvents="none" alt="flag" />
+              <span> {desktopView && (language ? "Es" : "En")}</span>
+            </Link>
         </div>
+        
     )
     
 }
