a31ee25dafba4573a8f9b74050d7e4a1e3079abb
zcley02 <zcleymerjoel16@gmail.com>
2024-09-08 20:25:34 -0600

    Last update of the deploy

--- src/utils/content.js
+++ src/utils/content.js
@@ -35,8 +35,8 @@ const jsp = {
 export const projects = [
     {
         title: "Journal App",
-        link: "",
-        github: "aa",
+        link: "https://journal-app-zc.netlify.app/",
+        github: "https://github.com/Zcley02/journal-app",
         gifProject: JournalApp,
         image: JournalApp ,
         description: {
@@ -49,6 +49,7 @@ export const projects = [
     {
         title: "Express API with Sequelize ORM",
         github: "",
+        githubBack: "https://github.com/Zcley02/api-sequelize",
         gifProject: Service,
         image: Service,
         description: {
@@ -60,7 +61,7 @@ export const projects = [
     },
     {
         title: "Arboreto Carmelo Palma Website",
-        github: "",
+        github: "https://github.com/Zcley02/arboret-carmelo-palma",
         gifProject: Tree,
         image: Tree,
         description: {
@@ -75,16 +76,16 @@ export const projects = [
 export const myExperiences = [
     {
         year: "2022 - 2024",
-        titleEsp: "",
-        titleEng: "",
-        textEsp : "",
-        textEng: ""
+        titleEsp: "Desarrolador Jr.",
+        titleEng: "Jr Developer",
+        textEsp : "Como Desarrollador Junior en el Instituto Nicaragüense de Seguros y Reaseguros (INISER), me especializo en el mantenimiento, gestión e implementación de procesos para el sistema Acsel/x utilizando Oracle Forms. También me encargo del diseño y mejora de reportes generados con Oracle Reports, facilitando la obtención de información valiosa para los usuarios. En el ámbito de la programación, utilizo Oracle PL/SQL para desarrollar y mantener procesos en objetos de base de datos. Además, tengo experiencia como desarrollador web, realizando mantenimiento y gestión de una página web diseñada con ASP Clásico, Microsoft y Crystal Reports. También gestiono un servicio de API REST desarrollado con Spring Boot.",
+        textEng: "As a Junior Developer at the Instituto Nicaragüense de Seguros y Reaseguros (INISER), I specialize in the maintenance, management, and implementation of processes for the Acsel/x system using Oracle Forms. I am also responsible for designing and improving reports generated with Oracle Reports, facilitating the extraction of valuable information for users. In programming, I use Oracle PL/SQL to develop and maintain processes in database objects. Additionally, I have experience as a web developer, performing maintenance and management of a website designed with Classic ASP, Microsoft technologies, and Crystal Reports. I also manage a REST API service developed with Spring Boot."
     },
     {
         year: "2018 - 2023",
-        titleEsp: "",
-        titleEng: "",
-        textEsp : "",
-        textEng: ""
+        titleEsp: "Estudiante de Ingenieria en Sistemas de Información",
+        titleEng: "Information Systems Engineering Student",
+        textEsp : "Estudie en la Universidad Centroamericana (UCA), donde me prepare profesionalmente en la carrera de Ingeniería en Sistemas de la Información. En el transcurro de mi estadía en la UCA, pude obtener tanto los conocimientos importantes de programación para poder desarrollar mis habilidades y lógica de programación. De igual manera, ser capaz de entender el proceso colaborativo en la diseño, análisis y gestión de proyectos de desarrollo.",
+        textEng: "I studied at the Central American University (UCA), where I received professional training in the field of Information Systems Engineering. During my time at UCA, I acquired essential programming knowledge that allowed me to develop my skills and programming logic. Additionally, I gained an understanding of the collaborative process involved in designing, analyzing, and managing development projects."
     }
 ]
